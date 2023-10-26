6232e7cff0ddc6a79f8e4f9d92dc3ebbcf213d4e
akash-appycodes <akash@appycodes.com>
2023-10-26 12:28:54 +0530

    idk cmmit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Here are my projects </div>;
+import { getProjects } from "@/sanity/sanity-utils";
+
+export default async function Home() {
+  const projects = await getProjects();
+  console.log(projects);
+  return (
+    <div>
+      {projects.map((project) => (
+        <div key={project._id}>{project.name}</div>
+      ))}
+    </div>
+  );
 }
